874d1f8b628571d41df2459cdb3847643c529457
Notham Capo <possidiusc@gmail.com>
2024-08-31 11:40:06 +0200

    page de resumé en cours

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -6,70 +6,218 @@ import {
   FaHtml5,
   FaCss3,
   FaJs,
+  FaPython,
+  FaJava,
+  FaPhp,
   FaReact,
   FaFigma,
-  FaNodeJs,
 } from "react-icons/fa";
 
-import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+import {
+  SiTailwindcss,
+  SiNextdotjs,
+  SiVuedotjs,
+  SiTypescript,
+  SiAngular,
+  SiLaravel,
+  SiSpringboot,
+  SiDjango,
+} from "react-icons/si";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
+
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+import { motion } from "framer-motion";
 
 // about data
 const about = {
   title: "About me",
   description:
     "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores cumque asperiores vero dolorem aliquam dicta? Expedita possimus nisi consectetur sed?",
-    info :[
-      {
-        fieldName: "Name",
-        fieldValue: "possi capo",
-      },
-      {
-        fieldName: "Phone",
-        fieldValue: "(+229) 66 957 003",
-      },
-      {
-        fieldName: "Experience",
-        fieldValue: "2+ Years",
-      },
-      {
-        fieldName: "Skype",
-        fieldValue: "Possidis",
-      },
-      {
-        fieldName: "Nationality",
-        fieldValue: "Beninois",
-      },
-      {
-        fieldName: "Email",
-        fieldValue: "possidiusc@gmail.com",
-      },
-      {
-        fieldName: "Freelance",
-        fieldValue: "Available",
-      },
-      {
-        fieldName: "Languages",
-        fieldValue: "French, English",
-      },
-    ]
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "possi capo",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "(+229) 66 957 003",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "2+ Years",
+    },
+    {
+      fieldName: "Skype",
+      fieldValue: "Possidis",
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Beninois",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "possidiusc@gmail.com",
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Available",
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: "French, English",
+    },
+  ],
 };
 
 // experience data
 const experiences = {
   icon: "/assets/resume/badge.svg",
   title: "My experience",
-  description: "",
-  items:[
+  description:
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores cumque asperiores vero dolorem aliquam dicta? Expedita possimus nisi consectetur sed?",
+  items: [
     {
-      company:"",
-      position:"",
-      duration:"",
-    }
-  ]
-}
+      company: "GUNIVERS",
+      position: "FullStack Developer",
+      duration: "2024 - Present",
+    },
+    {
+      company: "Freelance",
+      position: "Graphic Designer",
+      duration: "2022 - Present",
+    },
+  ],
+};
+
+// education data
+const education = {
+  icon: "/assets/resume/cap.svg",
+  title: "My education",
+  description:
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores cumque asperiores vero dolorem aliquam dicta? Expedita possimus nisi consectetur sed?",
+  items: [
+    {
+      institution: "GUNIVERS",
+      degree: "FullStack Developer",
+      duration: "2024 - Present",
+    },
+    {
+      institution: "GUNIVERS",
+      degree: "FullStack Developer",
+      duration: "2024 - Present",
+    },
+    {
+      institution: "GUNIVERS",
+      degree: "FullStack Developer",
+      duration: "2024 - Present",
+    },
+    {
+      institution: "GUNIVERS",
+      degree: "FullStack Developer",
+      duration: "2024 - Present",
+    },
+    {
+      institution: "GUNIVERS",
+      degree: "FullStack Developer",
+      duration: "2024 - Present",
+    },
+  ],
+};
+
+// skills data
+const skills = {
+  title: "My skills",
+  description:
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores cumque asperiores vero dolorem aliquam dicta? Expedita possimus nisi consectetur sed?",
+  skillList: [
+    {
+      icon: <FaHtml5 />,
+      name: "html5",
+    },
+    {
+      icon: <FaCss3 />,
+      name: "css3",
+    },
+    {
+      icon: <FaJs />,
+      name: "javasript",
+    },
+    {
+      icon: <FaJava />,
+      name: "java",
+    },
+    {
+      icon: <FaPython />,
+      name: "python",
+    },
+    {
+      icon: <FaPhp />,
+      name: "php",
+    },
+    {
+      icon: <FaReact />,
+      name: "react.js",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "tailwindcss",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "next.js",
+    },
+    {
+      icon: <SiAngular />,
+      name: "angular.js",
+    },
+    {
+      icon: <SiTypescript />,
+      name: "TypeScript",
+    },
+    {
+      icon: <SiVuedotjs />,
+      name: "vue.js",
+    },
+    {
+      icon: <SiLaravel />,
+      name: "laravel",
+    },
+    {
+      icon: <SiSpringboot />,
+      name: "springboot",
+    },
+    {
+      icon: <SiDjango />,
+      name: "django",
+    },
+  ],
+};
 
 const Resume: React.FC = () => {
-  return <div>Resume page</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs>
+          <TabsTrigger>Exprérience</TabsTrigger>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Resume;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,7 @@ const config = {
       sm: '640px',
       md: '768px',
       lg: '960px',
-      xl: '1200px',
+      xl: '1400px',
     },
     fontFamily: {
       primary: "var(--font-jetbrainsMono)"
